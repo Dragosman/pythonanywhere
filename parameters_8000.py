77c713ccf314d7b524d8436a77339ddca826d2b5
Dragos Manescu <dragos.v.manescu@gmail.com>
2015-09-02 16:30:20 +0300

    extra files

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$8dd3c469246ec0b9$cfbcc2054630ffff36e9675ab5b92ce3bdb43ee9"
+password="pbkdf2(1000,20,sha512)$8bac245bf9e0caa2$03ee6cc4fe40ab3247b60735c56bbcbe5b4cf825"
